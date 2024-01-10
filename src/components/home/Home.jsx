e6d7c31264edd1604ebe20f6cdd5cc85655f1bf9
sujataprasad01 <sujataprasad2004@gmail.com>
2024-01-10 18:42:40 +0530

    done with home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import './Home.css';
 import { Link } from 'react-scroll';
 import BannerImage from '../../assets/banner/ba01c2ad-b832-4a25-a16d-24f8225c8e6e.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartLine, faShieldHalved,faChartArea } from '@fortawesome/free-solid-svg-icons';
 function Home() {
+
+  const faChatIcon = <FontAwesomeIcon icon={faChartLine} />
+  const faShieldIcon = <FontAwesomeIcon icon={faShieldHalved} />
+  const faChartAreaIcon = <FontAwesomeIcon icon={faChartArea} />
+
+
+
   return (
     <>
       <section id='home'>
@@ -13,9 +22,9 @@ function Home() {
                 <h3 className='title'>
                    Leading <span className='rice' >Rice</span> Manufacturer and Exporter
                 </h3>
-                <h4>
-                Providing Quality Grains to Your Doorstep, Your <span className='rice' >Trusted</span> Rice <span className='rice' >Provider</span>, Anytime, Anywhere!
-                </h4>
+                <p>
+                Providing Quality Grains to Your Doorstep, <br/>Your <span className='rice' >Trusted</span> Rice <span className='rice' >Provider</span>, Anytime, Anywhere!
+                </p>
                 <div className='btn_wrapper'>
                     <Link className='btn' to="/">Discover More</Link>
                 </div>
@@ -25,10 +34,20 @@ function Home() {
                     <img src={BannerImage}></img>
                 </div>
                 <div className='banner_style_1'>
+                 {faChatIcon}
+                 <h4>Factory</h4>
+               
+                 {faShieldIcon}
+                 <h4>99.9% Sucess</h4>
+              
+                 {faChartAreaIcon}
+                 <h4>Strategy</h4>
+            </div>
+            <div className='sub_background'>
 
-                </div>
             </div>
             </div>
+         </div>
          </div>
         </div>
       </section>
