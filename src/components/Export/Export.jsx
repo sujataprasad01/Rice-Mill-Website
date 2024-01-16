dfdb49268035712ffc71690960ff98d0a04b9deb
sujataprasad01 <sujataprasad2004@gmail.com>
2024-01-16 23:33:02 +0530

    updated export images

--- src/components/Export/Export.jsx
+++ src/components/Export/Export.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLaptopCode, faChartBar, faCopy, faMarker, faUserGear, faCoins } from '@fortawesome/free-solid-svg-icons';
 import Paraboild from '../../assets/survices/ir-64-parboiled-rice-1626094215-5894204.jpeg';
 import White from '../../assets/survices/ir-64-non-basmati-parboiled-rice-500x500.webp';
-import Lachkari from '../../assets/survices/sona-masoori-rice-500x500.webp';
+import Lachkari from '../../assets/survices/20240116_101620.jpg';
 import Kali from '../../assets/survices/white-kali-much-rice-500x500.webp';
 import Heroone from '../../assets/survices/a3e627f8-873d-4dc9-8e5f-2187a4a32254.jpeg'
 import Herotwo from '../../assets/survices/a66d21e2-7da9-4e13-9ed6-8be61ca2f0b0.jpeg'
@@ -40,18 +40,8 @@ export default function Export() {
             {/* <div className="title_headlingg"> */}
      <h3>Exportation of Varieties of Rice</h3>
             {/* </div> */}
-                <div className="service_box">
-                    <div className="service_imgone"><img src={Paraboild}></img></div>
-                    {/* <h4 className="number">01</h4> */}
-                    <div className="service_content">
-                        <h5>IR 64 Parboiled Rice</h5>
-                        <p>Type: Parboiled<br/>
-Length: 6-7 mm<br/><br/>Experience the Perfect Blend of Nutrient-Rich Parboiled Goodness in Every Grain of IR 64.</p>
-                        {/* <a href="/" className="read">Read more</a> */}
-                    </div>
-                </div> 
-                <div className="service_box">
-                <div className="service_imgone"><img src={White}></img></div>
+            <div className="service_box">
+                <div className="service_imgone"><img src={Paraboild}></img></div>
 
                     {/* <h4 className="number">02</h4> */}
                     <div className="service_content">
@@ -63,19 +53,30 @@ Length: 6-7 mm<br/><br/>Pure Elegance on Your Plate: IR 64 Non-Basmati White Ric
                     </div>
                 </div>
                 <div className="service_box">
-                <div className="service_imgone"><img src={Lachkari}></img></div>
+                    <div className="service_imgone"><img src={White}></img></div>
+                    {/* <h4 className="number">01</h4> */}
+                    <div className="service_content">
+                        <h5>IR 64 Parboiled Rice</h5>
+                        <p>Type: Parboiled<br/>
+Length: 6-7 mm<br/><br/>Experience the Perfect Blend of Nutrient-Rich Parboiled Goodness in Every Grain of IR 64.</p>
+                        {/* <a href="/" className="read">Read more</a> */}
+                    </div>
+                </div> 
+               
+                <div className="service_box">
+                <div className="service_imgtwo"><img src={Lachkari}></img></div>
 
                     {/* <h4 className="number">03</h4> */}
                     <div className="service_content">
                         <h5>Lachkari Kolam</h5>
                         <p>Type: Non-Basmati<br/>
 Processing: Available in both raw and steam varieties<br/>
-Length: 6-7 mm<br/><br/>Versatile Delight: Lachkari Kolam, a Symphony of Taste Available in Both Raw and Steam Varieties.</p>
+<br/>Versatile Delight: Lachkari Kolam, a Symphony of Taste Available in Both Raw and Steam Varieties.</p>
                         {/* <a href="/" className="read">Read more</a> */}
                     </div>
                 </div>
                 <div className="service_box">
-                <div className="service_imgone"><img src={Kali}></img></div>
+                <div className="service_imgthree"><img src={Kali}></img></div>
 
                     {/* <h4 className="number">04</h4> */}
                     <div className="service_content">
